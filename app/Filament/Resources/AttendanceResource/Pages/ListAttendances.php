566f8eedf7a2da0b04b1ea0e76f2ee7a45d1a494
cloudmyn <cloudmyn46@gmail.com>
2024-08-11 02:05:55 +0800

    Menambahkan tabs di attendance

--- app/Filament/Resources/AttendanceResource/Pages/ListAttendances.php
+++ app/Filament/Resources/AttendanceResource/Pages/ListAttendances.php
@@ -5,7 +5,9 @@ namespace App\Filament\Resources\AttendanceResource\Pages;
 use App\Filament\Resources\AttendanceResource;
 use App\Filament\Widgets\AttendanceOverview;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListAttendances extends ListRecords
 {
@@ -23,6 +25,18 @@ class ListAttendances extends ListRecords
     {
         return [
             AttendanceOverview::class,
+        ];
+    }
+
+
+    public function getTabs(): array
+    {
+        return [
+            'Semua' => Tab::make(),
+            'Absen Masuk' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('type', 'check_in')),
+            'Absen Keluar' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('type', 'check_out')),
         ];
     }
 }
